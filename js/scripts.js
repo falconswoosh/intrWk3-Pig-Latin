d4a6a86284c3ad984700bf8bc20ce3e4ec0afda0
af <falconswoosh@gmail.com>
2017-09-20 23:59:31 -0700

    add logic if entire word has consonants e.g., the word dry

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,6 @@ var vowel = ["a","e","i","o","u"]
 
 var consonantCheck = function(input) {
   alert(input.charAt(1));
-  //
-  // for (var index = 0; index < vowel.length; index++) {
-  //   alert(vowel.length);
-  //   if (input.charAt(0) === vowel[index]) {
-  //
-  //   return input.slice(1,input.length) + input.slice(0,1) + "ay";
-  //
-  //   } else if (input.charAt(1) === vowel[index]) {
-  //
-  //   return input.slice(2,input.length) + input.slice(0,2) + "ay";
-  //
-  //   } else if (input.charAt(2) === vowel[index]) {
-  //
-  //   return input.slice(3,input.length) + input.slice(0,3) + "ay";
-  //
-  //   }
-  // }
 }
 
 var isNotALetter = function(input) {
@@ -33,34 +16,34 @@ var isNotALetter = function(input) {
 }
 
 var translateToASCII = function(input) {
-
+  var checkForQU = ["qu"];
   var notALetter = isNotALetter(input);
+  var PositionOfQU = input.indexOf(checkForQU[0]);
   if (notALetter) {
     alert("Not a letter");
   } else {
     if (vowel.indexOf(input[0]) > -1) {
       return input + "way";
+    } else if (PositionOfQU = 0) {
+      return input.slice(PositionOfQU+3,input.length) + input.slice(0,4) + "ay";
+    // } else if (PositionOfQU = 1) {
+    //   return input.slice(4,input.length) + input.slice(0,4) + "ay";
     } else if (vowel.indexOf(input[1]) > -1) {
       return input.slice(1,input.length) + input.slice(0,1) + "ay";
     } else if (vowel.indexOf(input[2]) > -1) {
       return input.slice(2,input.length) + input.slice(0,2) + "ay";
     } else if (vowel.indexOf(input[3]) > -1) {
       return input.slice(3,input.length) + input.slice(0,3) + "ay";
+    } else if (vowel.indexOf(input[4]) > -1) {
+      return input.slice(4,input.length) + input.slice(0,4) + "ay";
+    } else if (vowel.indexOf(input[5]) > -1) {
+      return input.slice(5,input.length) + input.slice(0,5) + "ay";
+    } else { //only do below if all letters are consanants (e.g., dry, rythyms, etc.)
+      return input.slice(input.length-1,input.length) + input.slice(0,input.length-1) + "ay";
       alert("hi");
-  }
-
-    // for (var index=0; index < vowel.length && ; index++) {
-    //   if (input.charAt(0) === vowel[index]) {
-    //     return input  + "way";
-    //   } else {
-    //     consonantCheck(input);
-    //       input = input + inputSliced + "ay";
-    //   }
-    // }
+   }
   }
 }
-//}
-
 
 // front end
 $(document).ready(function() {
